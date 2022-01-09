608838a059c16f2d986de6e107aa573a5a9852bb
avkuronen <97400505+avkuronen@users.noreply.github.com>
2022-01-09 20:24:28 +0300

    Update settings.py

--- QuadraticUnequationSolver/settings.py
+++ QuadraticUnequationSolver/settings.py
@@ -68,7 +68,7 @@ USE_L10N = True
 USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = [
-    BASE_DIR / "static/",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static/",
+# ]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
